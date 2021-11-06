7c8ca2522706efe330769b2dcd86275763228e37
ardy.putrautama53 <ardy.putrautama53@gmail.com>
2021-11-06 12:30:22 +0700

    Sidebar Login Fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { 
-  // useState,
-} from "react";
-import { 
-  BrowserRouter as Router, 
-  Switch,
-  // Redirect 
+import React from // useState,
+"react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    // Redirect
 } from "react-router-dom";
 import "tailwindcss/tailwind.css";
 import "./App.css";
@@ -23,71 +22,69 @@ import PublicRoute from "./components/PublicRoute";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  // const [value, setValue] = useState("");
-  const path = window.location.pathname;
+    // const [value, setValue] = useState("");
+    const path = window.location.pathname;
 
-  return (
-    // YANG DIPAKE:
-    <Router>
-      <div className="flex">
-        
-        <div className="h-screen sticky top-0">
-          {(path !== "/") && <Drawer />}
-        </div>
-          
-        {/* Main Content */}
-        <div className="flex-auto">
-          <Switch>
-            {APP_ROUTE.map((value, index) => {
-              if (value.private) {
-                return (
-                  <PrivateRoute 
-                    key={value.name}
-                    component={value.component} 
-                    path={value.path} 
-                    exact={value.exact}
-                  />
-                )
-              } else {
-                return (
-                  <PublicRoute 
-                    key={value.name}
-                    restricted={value.restricted}
-                    component={value.component} 
-                    path={value.path} 
-                    exact={value.exact}
-                  />
-                )
-              }
-            })}
-          </Switch>
-        </div>
+    return (
+        // YANG DIPAKE:
+        <Router>
+            <Switch>
+                {APP_ROUTE.map((value, index) => {
+                    if (value.private) {
+                        return (
+                            <div className="flex">
+                                <div className="h-screen sticky top-0">
+                                    <Drawer />
+                                </div>
 
-      </div>
-    </Router>
-      
-    // TESTING:
+                                {/* Main Content */}
+                                <div className="flex-auto">
+                                    <PrivateRoute
+                                        key={value.name}
+                                        component={value.component}
+                                        path={value.path}
+                                        exact={value.exact}
+                                    />
+                                </div>
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <PublicRoute
+                                key={value.name}
+                                restricted={value.restricted}
+                                component={value.component}
+                                path={value.path}
+                                exact={value.exact}
+                            />
+                        );
+                    }
+                })}
+            </Switch>
+        </Router>
 
-    // <Router>
-    //   <div className="relative min-h-screen md:flex">
-    //     {!(window.location.pathname === "/") && <Drawer />}
-    //     <div className={"flex-auto"}>
-    //       <Switch>
-    //         <Route path="/" component={Login} exact />
-    //           {/* <UserContext.Provider value={{ value, setValue }}> */}
-    //           <Route path="/appointment/create" component={AppointmentPage} exact />
-    //           <Route path="/appointment/history" component={AppointmentHistory} />
-    //           <Route path="/user/list" component={UserAdmin} exact/>
-    //           <Route path="/user/create" component={CreateUser} exact/>
-    //           <Route path="/user/update" component={UpdateUser} exact/>
-    //           <Route path="/guest/list" component={GuestAdmin} exact/>
-    //           <Route path="/testing" component={Testing} exact/>  
-    //           {/* </UserContext.Provider> */}
-    //       </Switch>
-    //     </div>
-    //   </div>
-    // </Router>
-  );
+        // TESTING:
+
+        // <Router>
+        //   <div className="relative min-h-screen md:flex">
+        //     {!(window.location.pathname === "/") && <Drawer />}
+        //     <div className={"flex-auto"}>
+        //       <Switch>
+        //         <Route path="/" component={Login} exact />
+        //           {/* <UserContext.Provider value={{ value, setValue }}> */}
+        //           <Route path="/appointment/create" component={AppointmentPage} exact />
+        //           <Route path="/appointment/history" component={AppointmentHistory} />
+        //           <Route path="/user/list" component={UserAdmin} exact/>
+        //           <Route path="/user/create" component={CreateUser} exact/>
+        //           <Route path="/user/update" component={UpdateUser} exact/>
+        //           <Route path="/guest/list" component={GuestAdmin} exact/>
+        //           <Route path="/testing" component={Testing} exact/>
+        //           {/* </UserContext.Provider> */}
+        //       </Switch>
+        //     </div>
+        //   </div>
+        // </Router>
+    );
 }
 
 export default App;
